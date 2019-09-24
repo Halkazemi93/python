5d05e870caa53afb94bc5d3c8b1c847d380c11b7
Hussain Al Kazemi <mhk.alk@gmail.com>
2019-09-24 19:08:06 +0300

    updated recruitment program

--- recruitment_program.py
+++ recruitment_program.py
@@ -6,13 +6,13 @@ skills = [
     'Critical Thinking', 
     'Running',
 ]
-cv = {'Name':' ' , 'Age':' ' , 'Experience':' ' , 'Skills':[]}
+cv = {'name':' ' , 'age':' ' , 'experience':' ' , 'skills':[]}
 
 print("""Welcome to the Recruitment Program! Please follow the instructions below
 """)
-cv['Name'] = input("Please write your name: ").title()
-cv['Age'] = int(input("Please write your age: "))
-cv['Experience'] = int(input("Please write your years of experience: "))
+cv['name'] = input("Please write your name: ").title()
+cv['age'] = int(input("Please write your age: "))
+cv['experience'] = int(input("Please write your years of experience: "))
 
 print("Please choose two skills from the following skills set below by typing the number corresponding to that skill:")
 
@@ -24,37 +24,18 @@ for skill in skills:
 option1 = int(input("Please choose your first skill: "))
 option2 = int(input("Please choose your second skill. It cannot be the same as the first skill: "))
 
-if option1 == 1:
-    cv['Skills'].append('Python')
-elif option1 == 2:
-    cv['Skills'].append('Django')
-elif option1 == 3:
-    cv['Skills'].append('JavaScript')
-elif option1 == 4:
-    cv['Skills'].append('Painting')
-elif option1 == 5:
-    cv['Skills'].append('Critical Thinking')
-elif option1 == 6:
-    cv['Skills'].append('Running')
+if option1 >= 1 and option1 <= len(skills):
+    cv["skills"].append(skills[int(option1) - 1])
 else:
     print("Not part of the list!")
 
-if option2 == 1:
-    cv['Skills'].append('Python')
-elif option2 == 2:
-    cv['Skills'].append('Django')
-elif option2 == 3:
-    cv['Skills'].append('JavaScript')
-elif option2 == 4:
-    cv['Skills'].append('Painting')
-elif option2 == 5:
-    cv['Skills'].append('Critical Thinking')
-elif option2 == 6:
-    cv['Skills'].append('Running')
+if option2 >= 1 and option2 <= len(skills):
+    cv['skills'].append(skills[int(option2) - 1])
 else:
     print("Not part of the list!")
 
-if cv['Age'] >= 21 and cv['Age'] <= 50 and cv['Experience'] >= 4 and ((option1 == 1) or (option2 == 1)):
-    print("Congratulations {}, you have been accepted to the program!!!!".format(cv['Name']))
+
+if cv['age'] >= 21 and cv['age'] <= 50 and cv['experience'] >= 4 and ((option1 == 1) or (option2 == 1)):
+    print("Congratulations {}, you have been accepted to the program!!!!".format(cv['name']))
 else:
     print("Unfortunately your credentials do not meet the requirements. Sorry you have been rejected")
